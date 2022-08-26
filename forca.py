fc90f0d35355df671b339cad3398ecb432008f8f
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-08-26 00:34:59 -0300

    arquivos ptyhon (Curso 1)

---
+++ forca.py
@@ -0,0 +1,18 @@
+def jogar():
+    print("***************************")
+    print("Bem vindo ao jogo da Forca!")
+    print("***************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        pass
+
+    print("Fim do jogo")
+
+
+if __name__ == '__main__':
+    jogar()
